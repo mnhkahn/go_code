1b304fd4310374968d6266bfc03922cbf494b59f
git <lichao@rd.tuan800.com>
2014-08-14 14:07:51 +0800

    reverse

--- reverse.go
+++ reverse.go
@@ -2,9 +2,13 @@ package main
 
 func Reverse(s string) string {
 	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
 	return string(r)
 }
 func main() {
 	a := "Hello, 世界"
+	println(a)
 	println(Reverse(a))
 }
