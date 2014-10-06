f6f45f0927576a8543ca7b765f2050a584a4bd09
Bryce <lichao@cyeam.com>
2014-10-06 10:30:32 +0800

    wallpaper

---
+++ wallpaper/app/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"github.com/astaxie/beego/httplib"
+	"go_code/wallpaper/win32api"
+)
+
+const bingURL = `http://cn.bing.com`
+const file_path = `C:\Users\Public\Pictures\wallpaper.jpg`
+
+func main() {
+	v := Bing{}
+	req := httplib.Get("http://www.bing.com/HPImageArchive.aspx?format=json&idx=0&n=1")
+	err := req.ToXml(&v)
+
+	if len(v.Images) > 0 && err == nil {
+		pic := httplib.Get(bingURL + v.Images[0].Url)
+		fmt.Println(pic)
+		// err = pic.ToFile(file_path)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println(win32api.SetWallPaper(file_path))
+}
+
+type Bing struct {
+	XMLName xml.Name `xml:"images"`
+	Images  []Image  `xml:"image"`
+}
+
+type Image struct {
+	XMLName       xml.Name `xml:"image"`
+	Startdate     string   `xml:"startdate"`
+	Fullstartdate string   `xml:"fullstartdate"`
+	Url           string   `xml:"url"`
+}
